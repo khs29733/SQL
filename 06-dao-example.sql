017aed322bf0f34aa6ad71a53dfd7b591233e793
khs29733 <79631666+khs29733@users.noreply.github.com>
2021-03-22 16:42:36 +0900

    Add files via upload

---
+++ 06-dao-example.sql
@@ -0,0 +1,44 @@
+--DAO 객체 생성 연습
+-- 사용자 계정으로 접속
+SELECT * FROM author CASCADE CONSTRAINTS;
+DROP TABLE book;
+SELECT * FROM user_objects;
+DROP SEQUENCE seq_author_id;
+
+CREATE TABLE author (
+    id NUMBER(10),
+    name VARCHAR2(50) NOT NULL,
+    bio VARCHAR2(100),
+    PRIMARY KEY(id)
+    );
+CREATE SEQUENCE seq_author
+    START WITH BY 1
+    MAXVALUE 1000000;
+    
+    SELECT * FROM user_objects;
+    
+    INSERT INTO author
+    VALUES(seq_author.NEXTVAL,
+    '박경리', '토지작가');
+SELECT * FROM author;
+
+COMMIT; -- 데이터베이스 반영
+
+--미니 프로젝트 SQL
+CREATE TABLE PHONE_BOOK(
+    id NUMBER(10) PRIMARY KEY,
+    name VARCHAR2(10),
+    hp VARCHAR2(20),
+    tel VARCHAR2(20)
+    );
+    
+CREATE SEQUENCE seq_phont_book;
+INSERT INTO phone_book
+VALUES(seq_phone_book.NEXTVAL, '고길동', '010-1234-5678', '02-986-5412');
+
+SELECT * FROM phone_book;
+COMMIT;
+
+
+
+
